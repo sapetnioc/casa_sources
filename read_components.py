ecf4a9e085f1740dfca747a7b214741c9dd2f077
sapetnioc <sapetnioc@users.noreply.github.com>
2021-10-06 17:10:31 +0200

    Read ordered list of vcstool definition file. Add a "brainvisa-cmake" key for filtering

---
+++ read_components.py
@@ -0,0 +1,17 @@
+import yaml
+from collections import OrderedDict
+
+def ordered_load(stream, Loader=yaml.SafeLoader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+
+components = ordered_load(open('components.yaml'))
+print(';'.join(name for name, component in components['repositories'].items() if component.get('brainvisa-cmake') != 'ignore'))
